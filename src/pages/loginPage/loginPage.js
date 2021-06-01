27c2ae27a9706d9237f4d26793035c7f3403f42a
Alex Domínguez Ortega <alex.dominguez-ortega@external.technipenergies.com>
2021-06-01 12:10:34 +0200

    Cambio de contraseña

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React,{ useState} from 'react';
 import Login from '../../components/login/login';
+import Alert from '@material-ui/lab/Alert';
+import Collapse from '@material-ui/core/Collapse'
+
 import './login.css';
 //Página de home con el menú para ir a las aplicaciones de isotracker
 
 const LoginPage = () =>{
     document.title= process.env.REACT_APP_APP_NAMEPROJ
+
     return(
         <body>
             <div className="home__background">
-                
                 
                 <Login/>
   );
