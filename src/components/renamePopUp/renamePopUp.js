90e07c9cb7dc2fb5f027ba40a1d24162ea273323
Alex Domínguez Ortega <alex.dominguez-ortega@external.technipenergies.com>
2021-06-07 10:54:31 +0200

    Fixes rename

--- src/components/renamePopUp/renamePopUp.js
+++ src/components/renamePopUp/renamePopUp.js
@@ -34,22 +34,22 @@ export default class RenamePopUp extends Component {
     render() {
         
         return (
-            <div style={{display:"inline-flex"}}>
-                <button class="btn btn-sm btn-danger" style={{fontSize:"12px",  padding:"2px 5px 2px 5px", marginRight:"5px", float:"left"}} onClick={() => this.openModal()}>LOCKED</button>
+            <section style={{display:"inline-block"}}>
+                <button type="button" class="btn btn-sm btn-danger" style={{fontSize:"12px",  padding:"2px 5px 2px 5px", marginRight:"5px"}} onClick={() => this.openModal()}>LOCKED</button>
                 <div>
                     <Modal visible={this.state.visible} width="550" height="100" effect="fadeInUp" onClickAway={() => this.closeModal()}>
                         <center style={{marginTop:"10px"}}>
                             <h4>{this.props.filename}</h4>
                         </center>
                         <div style={{marginTop:"10px", display:"inline-block", marginLeft:"30px"}}>  
-                            <textarea id="comments" placeholder="New name" class="comments" cols="51" rows="1" required="" maxlength="400" name="comments" style={{margin: "2px 10px 10px 10px", float:'left'}}></textarea>
+                            <textarea id="comments" defaultValue={this.props.filename} class="comments" cols="51" rows="1" required="" maxlength="400" name="comments" style={{margin: "2px 10px 10px 10px", float:'left'}}></textarea>
                             <button class="btn btn-sm btn-success" style={{fontSize:"15px", float:"left"}} onClick={()=>this.rename()}>Rename</button>
                         </div>
                                 
                             
                     </Modal>
                 </div>
-            </div>
+            </section>
         );
     }
 }
